5bf26a41fee523637311e77f3263739ed086bb04
ahmedfaiza01 <146230491+ahmedfaiza01@users.noreply.github.com>
2025-07-10 17:29:48 +0600

    Add files via upload

---
+++ BloodBank_Section(5BF)/C231466/Firebase.config.js
@@ -0,0 +1,36 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlp5WHC-AuEHFvtCW-Rf3IrnL9gKVTOMU",
+  authDomain: "blood-donation-e0029.firebaseapp.com",
+  projectId: "blood-donation-e0029",
+  storageBucket: "blood-donation-e0029.appspot.com",
+  messagingSenderId: "435211409787",
+  appId: "1:435211409787:web:06d08711d56e17ddbe45de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
