4568c9a20bf10c088406698d21adeba772a1d7f3
subahC231459 <subahmashiat15@gmail.com>
2025-08-03 23:40:45 +0600

    Add files via upload

---
+++ Vehicle_Servicing_5BF/showbill.php
@@ -0,0 +1,69 @@
+<?php
+   
+   $id= $_GET['id'];
+
+   $conn=mysqli_connect('localhost','root','','vehicle servicing');
+   $sql="SELECT * FROM bill WHERE id='$id'";
+   $result=mysqli_query($conn,$sql);
+
+   $bill=mysqli_fetch_assoc($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Welcome</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+    
+    <br><br><br>
+	 <?php include 'navbar_admin.php';?>
+     
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2">
+          <a class="btn btn-info" href="indexbill.php">Bill List</a>
+        </div> 
+        <div class="col-md-6">
+        <h2>Billing Information</h2>
+        <hr>
+          
+        <table class="table" >
+           
+          <tr>
+            <th >Service Charge:</th>
+            <td><?php echo $bill['salary']; ?></td>
+          </tr>
+          <tr>
+            <th >Equipment:</th>
+            <td><?php echo $bill['equipment']; ?></td>
+          </tr>
+          <tr>
+            <th >Oil:</th>
+            <td><?php echo $bill['oil']; ?></td>
+          </tr>
+          <tr>
+            <th >Total Cost:</th>
+            <td><?php echo $bill['tcost']; ?></td>
+          </tr>
+        </table>  
+
+        </div>
+      </div>
+    </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html> 
